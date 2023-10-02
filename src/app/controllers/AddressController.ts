eb7b737c7025cffe3ae789921250239f82592a7f
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-10-02 14:56:09 -0300

    feat(Address/Route): implements route to update address

--- src/app/controllers/AddressController.ts
+++ src/app/controllers/AddressController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
 import { findAddresses } from '../useCases/address/findAddresses';
+import { updateAddress } from '../useCases/address/updateAddress';
+import { addressSchema } from '../schemas/addressSchemas';
 
 class AddressController {
   async index(req: Request, res: Response) {
@@ -10,6 +12,23 @@ class AddressController {
     const addresses = await findAddresses(req.userId);
 
     res.json(addresses);
+  }
+
+  async update(req: Request, res: Response) {
+    const { userId } = req;
+    const { id } = req.params;
+
+    if (!userId) {
+      return res.sendStatus(404);
+    }
+    const addressData = addressSchema.parse(req.body);
+
+    const addressUpdated = await updateAddress({
+      addressId: id,
+      addressData,
+    });
+
+    res.status(204).json(addressUpdated);
   }
 }
 
