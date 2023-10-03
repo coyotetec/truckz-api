d021ebfcf6dc4ec86f344aa863559dec8d600c73
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-10-03 16:09:26 -0300

    feat(updateAddress): adds validation if address exists

--- src/app/useCases/address/updateAddress.ts
+++ src/app/useCases/address/updateAddress.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 import { addressSchema } from '../../schemas/addressSchemas';
 import AddressRepository from '../../repositories/AddressRepository';
+import { prisma } from '../../../libs/prisma';
+import { APPError } from '../../errors/APPError';
 
 interface IUpdateAddressArgs {
   addressId: string;
@@ -11,6 +13,16 @@ export async function updateAddress({
   addressId,
   addressData,
 }: IUpdateAddressArgs) {
+  const address = await prisma.address.findUnique({
+    where: {
+      id: addressId,
+    },
+  });
+
+  if (!address) {
+    throw new APPError('address does not exists');
+  }
+
   const addressUpdated = await AddressRepository.update({
     where: {
       id: addressId,
