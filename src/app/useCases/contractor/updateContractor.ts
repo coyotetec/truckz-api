b62fe3204b224834ce70cd861a1a9c430819cb28
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-10-05 17:42:58 -0300

    feat(updateContractor): adds validation of cnpj and cpf

--- src/app/useCases/contractor/updateContractor.ts
+++ src/app/useCases/contractor/updateContractor.ts
@@ -21,6 +21,27 @@ export async function updateContractor(
     throw new APPError('contractor does not exists');
   }
 
+  const contractorAlreadyExists = await ContractorRepository.findFirst({
+    where: {
+      OR: [{ cnpj: payload.cnpj }, { cpf: payload.cpf }],
+    },
+  });
+
+  if (contractorAlreadyExists) {
+    const sameFields = [
+      ...(payload.cpf && contractorAlreadyExists?.cpf === payload.cpf
+        ? ['cpf']
+        : []),
+      ...(payload.cnpj && contractorAlreadyExists?.cnpj === payload.cnpj
+        ? ['cnpj']
+        : []),
+    ];
+
+    throw new APPError(
+      `the following fields are already in use: ${sameFields.join(', ')}`,
+    );
+  }
+
   const { contractorId } = contractor;
   console.log(contractorId);
   const contractorUpdated = await ContractorRepository.update({
